2af92adb1319ad083d48b0b9f3655fae3ea521e9
Admin <admin@MacBook-Pro-de-Admin.local>
2022-01-27 12:36:52 -0500

    añade clausula try

--- routes/auth.js
+++ routes/auth.js
@@ -8,28 +8,32 @@ var crypto = require('crypto');
 
 
 route.post('/login',async(req,res)=>{
+    try {
+        const user= await userModel.findOne({correo:req.body.correo}) 
+        if(!user)return res.status(400).json({ body:{
+            error:"Validación Usuario",
+            descripcion :'Usuario o contraseña Inválidos'
+        }})
+        /**pensiende validacion de la contraseña */
+        var hashPassword = crypto.createHash('md5').update(req.body.contrasena).digest('hex');
     
-    const user= await userModel.findOne({correo:req.body.correo}) 
-    if(!user)return res.status(400).json({ body:{
-        error:"Validación Usuario",
-        descripcion :'Usuario o contraseña Inválidos'
-    }})
-    /**pensiende validacion de la contraseña */
-    var hashPassword = crypto.createHash('md5').update(req.body.contrasena).digest('hex');
-
-    if(hashPassword!=user.contrasena)return res.status(400).json({ body:{
-        error:"Validación Usuario",
-        descripcion :'Usuario o contraseña Inválidos'
-    }})
+        if(hashPassword!=user.contrasena)return res.status(400).json({ body:{
+            error:"Validación Usuario",
+            descripcion :'Usuario o contraseña Inválidos'
+        }})
+        
+        const token=jwt.sign({_id:user._id},process.env.SECRET)
+        user.contrasena=undefined;
+        res.json({
+            body:{
+                user:user,
+                token:token
+            }
+        });
+    } catch (err) {
+        res.send(err.message)
+    }
     
-    const token=jwt.sign({_id:user._id},process.env.SECRET)
-    user.contrasena=undefined;
-    res.json({
-        body:{
-            user:user,
-            token:token
-        }
-    });
 })
 
 module.exports=route
