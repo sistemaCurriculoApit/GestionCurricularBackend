dfc2aaa967b5d4cdc845fcb23754dba832155451
admin <sistemaCurriculoApit@gmail.com>
2022-07-07 20:59:36 -0500

    control de errores controlados en creacion de usuario

--- routes/user.js
+++ routes/user.js
@@ -27,54 +27,33 @@ route.get('/user/token', async (req, res) => {
     res.send(token)
 })
 route.post('/user/add', verifyToken, async (req, res) => {
-    const usuarioValidar = await userModel.findOne({ correo: req.body.correo })
-    if (usuarioValidar) return res.status(400).json({
-        error: "Validación Datos",
-        descripcion: 'El Correo ya existe'
-    });
-
-    const estudiante = await estudianteModel.findOne({correo: req.body.correo})
-
-    if (req.body.rolId === userProfilesObject.est.id && !estudiante){
-        const estudiante = new estudianteModel({
-            identificacion: req.body.identificacion,
-            nombre: req.body.nombreUsuario,
-            universidad: req.body.universidadEstudiante,
-            universidadOrigen: req.body.universidadEstudianteOrigen,
-            programa: req.body.programa,
-            plan: req.body.plan,
-            programaOrigen: req.body.programaOrigen,
-            planOrigen: req.body.planOrigen,
-            correo : req.body.correo,
-            fechaActualizacion: new Date(),
-            fechaCreacion: new Date(),
-            estado: true,
-        })
-        try{
-            const saveEstudiante = await estudiante.save();
-        }
-        catch (error){
-            return res.status(500).json({
-                error:true,
-                descripcion: error.message
+    
+    try {
+        const usuarioValidar = await userModel.findOne({ correo: req.body.correo })
+        if (usuarioValidar) return res.status(400).json({
+            error: "Validación Datos",
+            descripcion: 'El Correo ya existe'
+        });
+    
+        const estudiante = await estudianteModel.findOne({correo: req.body.correo})
+    
+        if (req.body.rolId === userProfilesObject.est.id && !estudiante){
+            const estudiante = new estudianteModel({
+                identificacion: req.body.identificacion,
+                nombre: req.body.nombreUsuario,
+                universidad: req.body.universidadEstudiante,
+                universidadOrigen: req.body.universidadEstudianteOrigen,
+                programa: req.body.programa,
+                plan: req.body.plan,
+                programaOrigen: req.body.programaOrigen,
+                planOrigen: req.body.planOrigen,
+                correo : req.body.correo,
+                fechaActualizacion: new Date(),
+                fechaCreacion: new Date(),
+                estado: true,
             })
-        }
-    }else if (req.body.rolId === userProfilesObject.est.id){
-            estudiante.nombre= req.body.nombreUsuario;
-            estudiante.universidad= req.body.universidadEstudiante;
-            estudiante.universidadOrigen = req.body.universidadEstudianteOrigen;
-            estudiante.programa= req.body.programa;
-            estudiante.plan=req.body.plan;
-            estudiante.programaOrigen= req.body.programaOrigen;
-            estudiante.planOrigen=req.body.planOrigen;
-            estudiante.fechaActualizacion= new Date();
-            estudiante.estado = true;
             try{
-                const updateEstudiante = await estudianteModel.updateOne({
-                    _id: estudiante._id
-                }, {
-                    $set: { ...estudiante }
-                })
+                const saveEstudiante = await estudiante.save();
             }
             catch (error){
                 return res.status(500).json({
@@ -82,32 +61,60 @@ route.post('/user/add', verifyToken, async (req, res) => {
                     descripcion: error.message
                 })
             }
-    }
-
-    var hashPassword = crypto.createHash('md5').update(req.body.contrasena).digest('hex');
-    const user = new userModel({
-        nombreUsuario: req.body.nombreUsuario,
-        identificacion: req.body.identificacion,
-        correo: req.body.correo,
-        contrasena: hashPassword,
-        rolId: req.body.rolId,
-        fechaCreacion: new Date(),
-        fechaActualizacion: new Date(),
-        estado: true
-
-    })
-
-    const save = await user.save();
-    try {
-        res.send(save);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            error: true,
+        }else if (req.body.rolId === userProfilesObject.est.id){
+                estudiante.nombre= req.body.nombreUsuario;
+                estudiante.universidad= req.body.universidadEstudiante;
+                estudiante.universidadOrigen = req.body.universidadEstudianteOrigen;
+                estudiante.programa= req.body.programa;
+                estudiante.plan=req.body.plan;
+                estudiante.programaOrigen= req.body.programaOrigen;
+                estudiante.planOrigen=req.body.planOrigen;
+                estudiante.fechaActualizacion= new Date();
+                estudiante.estado = true;
+                try{
+                    const updateEstudiante = await estudianteModel.updateOne({
+                        _id: estudiante._id
+                    }, {
+                        $set: { ...estudiante }
+                    })
+                }
+                catch (error){
+                    return res.status(500).json({
+                        error:true,
+                        descripcion: error.message
+                    })
+                }
+        }
+    
+        var hashPassword = crypto.createHash('md5').update(req.body.contrasena).digest('hex');
+        const user = new userModel({
+            nombreUsuario: req.body.nombreUsuario,
+            identificacion: req.body.identificacion,
+            correo: req.body.correo,
+            contrasena: hashPassword,
+            rolId: req.body.rolId,
+            fechaCreacion: new Date(),
+            fechaActualizacion: new Date(),
+            estado: true
+    
+        })
+    
+        const save = await user.save();
+        try {
+            res.send(save);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                error: true,
+                descripcion: error.message
+            })
+        }    
+    }catch (error){
+        return res.status(500).json({
+            error:true,
             descripcion: error.message
         })
     }
-
 })
 route.get('/user/all', verifyToken, async (req, res) => {
     try {
