6699f8ef797b95246ee6be9c24e9567d621d70e5
Zeitgeist <shecksk8nick@gmail.com>
2022-10-20 22:46:29 -0500

    Standarization of paths /advancements

--- routes/advancements.js
+++ routes/advancements.js
@@ -3,6 +3,135 @@ const avanceModel = require('../models/avance')
 const docenteModel = require('../models/docente')
 const { paginationSize } = require('../constants/constants')
 
+router.get('/', async (req, res) => {
+  try {
+    const pageNumber = req.query.page || 0;
+    const search = req.query.search;
+    const dateCreationFrom = req.query.dateCreationFrom;
+    const dateCreationTo = req.query.dateCreationTo;
+    const query = {}
+
+    if (dateCreationFrom || dateCreationTo) {
+      query.fechaCreacion = {}
+      if (dateCreationFrom) {
+        query.fechaCreacion.$gte = new Date(new Date(dateCreationFrom).toDateString()).toISOString();
+      }
+      if (dateCreationTo) {
+        query.fechaCreacion.$lte = new Date(new Date(dateCreationTo).toDateString()).toISOString();
+      }
+    }
+
+    if (search) {
+      const regex = new RegExp(search, 'ig');
+      const or = {
+        $or: [
+          { 'descripcion': regex },
+        ]
+      }
+      query['$and'] = [query, or];
+    }
+
+    const advancementsPromise = avanceModel.find(query)
+      .skip(pageNumber > 0 ? (pageNumber * paginationSize) : 0)
+      .limit(paginationSize).sort({ fechaCreacion: -1 });
+
+    const advancementsCountPromise = avanceModel.count(query);
+
+    const [advancements, advancementsCount] = await Promise.all([advancementsPromise, advancementsCountPromise])
+    res.status(200).json({ advancements, advancementsCount })
+  } catch (error) {
+    res.status(400).json({
+      error: true,
+      description: error.message
+    })
+  }
+})
+
+router.get('/professor', async (req, res) => {
+  try {
+    const pageNumber = req.query.page || 0;
+    const { search, dateCreationFrom, dateCreationTo, email: correo } = req.query
+    const professor = await docenteModel.findOne({ correo })
+    let query = {}
+
+    if (!professor) {
+      return res.status(400).json({
+        error: "Wrong data provided",
+        description: 'Professor does not exist'
+      });
+    }
+
+    if (dateCreationFrom || dateCreationTo) {
+      query.fechaCreacion = {}
+      if (dateCreationFrom) {
+        query.fechaCreacion.$gte = new Date(new Date(dateCreationFrom).toDateString()).toISOString();
+      }
+      if (dateCreationTo) {
+        query.fechaCreacion.$lte = new Date(new Date(dateCreationTo).toDateString()).toISOString();
+      }
+    }
+
+    if (search) {
+      var regex = new RegExp(search, 'ig');
+      const or = {
+        $or: [
+          { 'descripcion': regex },
+        ]
+      }
+      query = {
+        $and: [query, or],
+        $and: [{ 'docenteId': professor._id }]
+      };
+    } else {
+      query = {
+        $and: [{ 'docenteId': professor._id }]
+      };
+    }
+
+
+    const advancementsPromise = avanceModel.find(query)
+      .skip(pageNumber > 0 ? (pageNumber * paginationSize) : 0)
+      .limit(paginationSize).sort({ fechaCreacion: -1 });
+
+    const advancementsCountPromise = avanceModel.count(query);
+
+    const [advancements, advancementsCount] = await Promise.all([advancementsPromise, advancementsCountPromise])
+    res.status(200).json({ advancements, advancementsCount })
+  } catch (error) {
+    res.status(400).json({
+      error: true,
+      description: error.message
+    })
+  }
+})
+
+router.get('/professor/:id', async (req, res) => {
+  try {
+    let query = {}
+    const { id } = req.params;
+    const { advancementYear, period, page, pageSize } = req.query;
+    const pageNumber = page || 0;
+    const paginationSizeLocal = pageSize || paginationSize;
+    const year = new Date(`${advancementYear}-1-1`).toISOString()
+
+    query = { añoAvance: { $eq: year }, periodo: { $eq: period }, docenteId: { $eq: id } }
+
+    const advancementsPromise = avanceModel.find(query)
+      .skip(pageNumber > 0 ? (pageNumber * paginationSize) : 0)
+      .limit(paginationSizeLocal).sort({ fechaCreacion: -1 });
+
+    const advancementsCountPromise = avanceModel.count(query);
+
+    const [advancements, advancementsCount] = await Promise.all([advancementsPromise, advancementsCountPromise])
+    res.status(200).json({ advancements, advancementsCount })
+  } catch (error) {
+    res.status(400).json({
+      error: true,
+      description: error.message
+    })
+  }
+})
+
 router.post('/add', async (req, res) => {
   try {
     const avance = new avanceModel({
@@ -30,115 +159,6 @@ router.post('/add', async (req, res) => {
     });
   }
 
-})
-
-router.get('/all', async (req, res) => {
-
-  try {
-    let pageNumber = req.query.page ? req.query.page * 1 : 0;
-    let query = {}
-
-    //Datos para los filtros
-    let search = req.query.search;
-    let dateCreationFrom = req.query.dateCreationFrom;
-    let dateCreationTo = req.query.dateCreationTo;
-
-
-    if (dateCreationFrom || dateCreationTo) {
-      query.fechaCreacion = {}
-      if (dateCreationFrom) {
-        query.fechaCreacion.$gte = new Date(new Date(dateCreationFrom).toDateString()).toISOString();
-      }
-      if (dateCreationTo) {
-        query.fechaCreacion.$lte = new Date(new Date(dateCreationTo).toDateString()).toISOString();
-      }
-    }
-
-    if (search) {
-      var regex = new RegExp(search, 'ig');
-      const or = {
-        $or: [
-          { 'descripcion': regex },
-        ]
-      }
-      query = {
-        $and: [query, or],
-      };
-    }
-
-
-    const avances = await avanceModel.find(query)
-      .skip(pageNumber > 0 ? (pageNumber * paginationSize) : 0)
-      .limit(paginationSize).sort({ fechaCreacion: -1 });
-
-    const totalAvances = await avanceModel.count(query);
-    res.send({ avances, totalAvances })
-  } catch (error) {
-    res.status(400).json({
-      error: true,
-      descripcion: error.message
-    })
-  }
-})
-
-router.get('/allByDocenteEmail', async (req, res) => {
-
-  try {
-    let pageNumber = req.query.page ? req.query.page * 1 : 0;
-    let query = {}
-
-    //Datos para los filtros
-    let search = req.query.search;
-    let dateCreationFrom = req.query.dateCreationFrom;
-    let dateCreationTo = req.query.dateCreationTo;
-
-    let docente = await docenteModel.findOne({ correo: req.query.emailDocente })
-
-    if (!docente) return res.status(400).json({
-      error: "Validación Datos",
-      descripcion: 'El docente no existe'
-    });
-
-    if (dateCreationFrom || dateCreationTo) {
-      query.fechaCreacion = {}
-      if (dateCreationFrom) {
-        query.fechaCreacion.$gte = new Date(new Date(dateCreationFrom).toDateString()).toISOString();
-      }
-      if (dateCreationTo) {
-        query.fechaCreacion.$lte = new Date(new Date(dateCreationTo).toDateString()).toISOString();
-      }
-    }
-
-    if (search) {
-      var regex = new RegExp(search, 'ig');
-      const or = {
-        $or: [
-          { 'descripcion': regex },
-        ]
-      }
-      query = {
-        $and: [query, or],
-        $and: [{ 'docenteId': docente._id }]
-      };
-    } else {
-      query = {
-        $and: [{ 'docenteId': docente._id }]
-      };
-    }
-
-
-    const avances = await avanceModel.find(query)
-      .skip(pageNumber > 0 ? (pageNumber * paginationSize) : 0)
-      .limit(paginationSize).sort({ fechaCreacion: -1 });
-
-    const totalAvances = await avanceModel.count(query);
-    res.send({ avances, totalAvances })
-  } catch (error) {
-    res.status(400).json({
-      error: true,
-      descripcion: error.message
-    })
-  }
 })
 
 router.get('/allNotPaginated', async (req, res) => {
@@ -192,39 +212,6 @@ router.post('/allByAsignatura', async (req, res) => {
     let asignaturaId = req.body.asignaturaId;
 
     query = { añoAvance: { $eq: añoAvance }, periodo: { $eq: periodo }, asignaturaId: { $eq: asignaturaId } }
-
-    const avances = await avanceModel.find(query).skip(pageNumber > 0 ? (pageNumber * paginationSizeLocal) : 0)
-      .limit(paginationSizeLocal).sort({ fechaCreacion: -1 });
-
-    const totalAvances = await avanceModel.count(query);
-
-    res.status(200).json({
-      error: false,
-      descripcion: "Consulta Exitosa",
-      avances,
-      totalAvances
-    })
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: true,
-      descripcion: error.message
-    })
-  }
-})
-
-router.post('/allByDocente', async (req, res) => {
-  try {
-    let query = {}
-    let pageNumber = req.body.page ? req.body.page * 1 : 0;
-    let paginationSizeLocal = req.body.paginationSize || paginationSize;
-
-    //Datos para los filtros
-    let añoAvance = req.body.añoAvance;
-    let periodo = req.body.periodo;
-    let docenteId = req.body.docenteId;
-
-    query = { añoAvance: { $eq: añoAvance }, periodo: { $eq: periodo }, docenteId: { $eq: docenteId } }
 
     const avances = await avanceModel.find(query).skip(pageNumber > 0 ? (pageNumber * paginationSizeLocal) : 0)
       .limit(paginationSizeLocal).sort({ fechaCreacion: -1 });
