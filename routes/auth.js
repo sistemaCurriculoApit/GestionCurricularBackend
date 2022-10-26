d6cf448f18d7a7b37907d1a99d316c139e90461b
Zeitgeist <shecksk8nick@gmail.com>
2022-10-25 22:25:52 -0500

    Linted files

--- routes/auth.js
+++ routes/auth.js
@@ -1,44 +1,46 @@
-const express =require('express')
-const route=express.Router()
+const express = require('express');
+const route = express.Router();
 
-const userModel=require('../models/user')
-const jwt= require('jsonwebtoken')
+const userModel = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-var crypto = require('crypto');
-
+const crypto = require('crypto');
 
 route.post('/login', async (req, res) => {
-    try {
-        const user = await userModel.findOne({ correo: req.body.correo })
-        if (!user) return res.status(400).json({
-            body: {
-                error: "Validación Usuario",
-                descripcion: 'Usuario o contraseña Inválidos'
-            }
-        })
-        /**si la autenticación fue con google identity se salta la validacion de la contraseña */
-        var hashPassword = crypto.createHash('md5').update(req.body.contrasena).digest('hex');
-        if (!req.body.googleIdentity) {
-            if (hashPassword != user.contrasena) return res.status(400).json({
-                body: {
-                    error: "Validación Usuario",
-                    descripcion: 'Usuario o contraseña Inválidos'
-                }
-            })
+  try {
+    const user = await userModel.findOne({ correo: req.body.correo });
+    if (!user) {
+      return res.status(400).json({
+        body: {
+          error: 'Validación Usuario',
+          descripcion: 'Usuario o contraseña Inválidos'
         }
-
-        const token = jwt.sign({ _id: user._id }, process.env.SECRET)
-        user.contrasena = undefined;
-        res.json({
-            body: {
-                user: user,
-                token: token
-            }
+      });
+    }
+    /** si la autenticación fue con google identity se salta la validacion de la contraseña */
+    const hashPassword = crypto.createHash('md5').update(req.body.contrasena).digest('hex');
+    if (!req.body.googleIdentity) {
+      if (hashPassword !== user.contrasena) {
+        return res.status(400).json({
+          body: {
+            error: 'Validación Usuario',
+            descripcion: 'Usuario o contraseña Inválidos'
+          }
         });
-    } catch (err) {
-        res.send(err.message)
+      }
     }
 
-})
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    user.contrasena = undefined;
+    res.json({
+      body: {
+        user,
+        token
+      }
+    });
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 
-module.exports = route ;
+module.exports = route;
