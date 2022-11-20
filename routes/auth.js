fdc98d2e0853a614784216ab442d553ae27ae53c
JuanPrat <33468928+JuanPrat@users.noreply.github.com>
2022-11-20 15:50:45 -0500

    eliminado de codigo para iniciar sesion solo con google identity (#40)

--- routes/auth.js
+++ routes/auth.js
@@ -13,25 +13,11 @@ route.post('/login', async (req, res) => {
       return res.status(400).json({
         body: {
           error: 'Validación Usuario',
-          descripcion: 'Usuario o contraseña Inválidos'
+          descripcion: 'Usuario Inválido'
         }
       });
     }
-    /** si la autenticación fue con google identity se salta la validacion de la contraseña */
-    const hashPassword = crypto.createHash('md5').update(req.body.contrasena).digest('hex');
-    if (!req.body.googleIdentity) {
-      if (hashPassword !== user.contrasena) {
-        return res.status(400).json({
-          body: {
-            error: 'Validación Usuario',
-            descripcion: 'Usuario o contraseña Inválidos'
-          }
-        });
-      }
-    }
-
     const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-    user.contrasena = undefined;
     res.json({
       body: {
         user,
